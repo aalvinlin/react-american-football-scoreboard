ff1e8618aec23985d136708744f04f036ded8c36
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2019-12-09 17:42:18 -0800

    updating downs and quarters finished

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
   const [scoreHistory, setScoreHistory] = useState([]);
+
+  const [currentDown, setCurrentDown] = useState(1);
+  const [yardsToGo, setYardsToGo] = useState(10);
+  const [ballOn, setBallOn] = useState(50);
+  const [currentQuarter, setCurrentQuarter] = useState(1);
+  
   const [message, setMessage] = useState("New game started. Scoreboard is live.");
   const [messagesBackground, setMessagesBackground] = useState("defaultBg");
+  
+  const [possessionTeam, setPossessionTeam] = useState("home");
 
   return (
     <div className="container">
@@ -28,7 +36,7 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow currentDown={currentDown} yardsToGo={yardsToGo} possessionTeam={possessionTeam} ballOn={ballOn} currentQuarter={currentQuarter}/>
       </section>
 
       <section className="messages">
@@ -45,10 +53,13 @@ function App() {
           <button className="homeButtons__button" onClick={() => { updateScoreboard("home", "safety"); }}>Safety</button>
         </div>
         
-        <div className="undoResetButtons">
+        <div className="controlButtons">
         <h3>Controls</h3>
-          <button className="undo" onClick={ () => undo() }>Undo</button>
-          <button className="reset" onClick={() => reset() }>Reset</button>
+          <button className="controlButtons__button" onClick={ () => undo() }>Undo</button>
+          <button className="controlButtons__button" onClick={() => reset() }>Reset</button>
+          <button className="controlButtons__button" onClick={() => nextDown() }>+1 Down</button>
+          <button className="controlButtons__button" onClick={() => changePossession() }>Change Possession</button>
+          <button className="controlButtons__button" onClick={() => nextQuarter() }>+1 Quarter</button>
         </div>
         
         <div className="awayButtons">
@@ -68,6 +79,32 @@ function App() {
     {
       let scoreTypes = {"touchdown": 6, "one-point conversion": 1, "two-point conversion": 2, "field goal": 3, "safety": 2};
       let pointsToAdd = scoreTypes[scoreType];
+      let isValidPlay = false;
+
+      // make sure that the last move was a touchdown before adding a one-point or two-point conversion
+      if (scoreType === "one-point conversion" || scoreType === "two-point conversion")
+      {
+        let [lastTeam, lastScore, lastMoveType] = scoreHistory[scoreHistory.length - 1];
+
+        console.log("Attempting a ", scoreType, "with the last team, ", lastTeam, " scoring a ", lastMoveType);
+        console.log(scoreHistory);
+
+        // make sure that the previous move was a touchdown from the same team
+        if (team === lastTeam && lastMoveType === "touchdown")
+          { isValidPlay = true;}
+        else if (scoreType === "one-point conversion")
+          {
+            setMessage(`Error: One-point conversions can only be scored directly after a touchdown.`);
+            setMessagesBackground("errorBg");
+            return;
+          }
+        else
+          {
+            setMessage(`Error: Two-point conversions can only be scored directly after a touchdown.`);
+            setMessagesBackground("errorBg");
+            return;
+          }
+      }
 
       // update score
       if (team === "away")
@@ -129,6 +166,66 @@ function App() {
     setMessagesBackground("defaultBg");
 
     
+  }
+
+  function nextDown() {
+
+    if (currentDown < 4)
+      {
+          let newDown = currentDown + 1;
+          setCurrentDown(newDown);
+          setMessage(`Down ${newDown} with ${yardsToGo} yards to go!`);
+          setMessagesBackground(possessionTeam + "Bg");
+      }
+    else
+      {
+        setMessage(`No more downs available for this team!`);
+        setMessagesBackground("errorBg");
+      }
+  }
+
+  function nextQuarter() {
+
+    if (currentQuarter < 4)
+      {
+          let nextQuarter = currentQuarter + 1;
+          setCurrentQuarter(nextQuarter);
+          setMessage(`Quarter ${nextQuarter} has started.`);
+          setMessagesBackground("defaultBg");
+
+          // reset other variables
+          setPossessionTeam("home");
+          setCurrentDown(1);
+          setYardsToGo(10);
+      }
+    else
+      {
+        setMessage(`This is the final quarter of the game!`);
+        setMessagesBackground("errorBg");
+      }
+  }
+
+
+  function changePossession() {
+    
+    // switch teams
+    let newTeam = undefined;
+    
+    if (possessionTeam === "home")
+      { newTeam = "away"; }
+    else
+      { newTeam = "home"; }
+
+    setPossessionTeam(newTeam);
+    
+      let newDown = 1;
+      let newYardsToGo = 10;
+
+      setCurrentDown(newDown);
+      setYardsToGo(newYardsToGo);
+
+      setMessage(`The ${newTeam} team has taken control of the ball. Down ${newDown} with ${newYardsToGo} yards to go!`);
+      setMessagesBackground(newTeam + "Bg");
   }
   
 }
